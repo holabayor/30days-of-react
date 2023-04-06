198a79230586ea62e2d0fc5098e99de0e1810a98
holabayor <liasu.olabayo@gmail.com>
2023-04-06 18:58:21 +0100

    set up

--- day-2/image-carousel/src/App.js
+++ day-2/image-carousel/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, { useState, useLayoutEffect } from 'react';
 import './App.css';
+import Carousel from './components/Carousel';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [images, setImages] = useState([]);
+
+  useLayoutEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((response) => {
+        setImages(response.products);
+      });
+  }, [images]);
+  return <div>{images && <Carousel images={images} />}</div>;
 }
 
 export default App;
---
+++ day-2/image-carousel/src/components/Carousel.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+export default function Carousel({ images }) {
+  const [current, setCurrent] = useState(0);
+  const prevSlide = (e) => {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  };
+
+  const nextSlide = (e) => {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  };
+  return (
+    <div className="container">
+      <h2>Image Carousel</h2>
+      <div className="carousel">
+        <img src={images[current].images[0]} alt="placeholder" />
+      </div>
+
+      <div className="buttons">
+        <button className="btn" name="previous" onClick={prevSlide}>
+          Previous
+        </button>
+        <button className="btn" name="next" onClick={nextSlide}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
