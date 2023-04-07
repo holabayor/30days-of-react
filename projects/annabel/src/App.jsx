a0e131292b45748f9d8daf30fe2e89a38c8a8dc8
holabayor <liasu.olabayo@gmail.com>
2023-04-07 17:16:21 +0100

    set up

---
+++ projects/annabel/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+       Header goes here
+      </div>
+      <div className="card">
+      </div>
+      <p className="read-the-docs">
+        Body goes here
+      </p>
+    </div>
+  )
+}
+
+export default App
